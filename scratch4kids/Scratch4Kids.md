af5ffd58a2d0cbf90f89d70b4864cdebf56c302e
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2024-03-24 13:53:06 -0400

    initialize course

---
+++ scratch4kids/Scratch4Kids.md
@@ -0,0 +1,116 @@
+ Scratch for Kids
+
+Author: Derek Breen (2nd Degree Black Belt in Scratch Ninjitsu)
+
+Amazon.ca: https://www.amazon.ca/Scratch-Kids-Dummies-Derek-Breen/dp/1119014875/
+
+Indigo.ca: https://www.indigo.ca/en-ca/scratch-for-kids-for-dummies/9781119014874.html
+
+
+# Part 0: Introduction
+
+## Get FreePlane
+
+## About Scratch
+
+### 1. Give you powerful software for free
+
+### 2. Make it easy for you to learn
+
+### 3. Allow many different ways for you to use the software
+
+### 4. Enable you to browse/play/remix other projects
+
+### 5. Enable you to share your projects
+
+### 6. Create an online community where you can learn from one another
+
+## View the Book
+
+# Part 1: Become a Scratch Designer
+
+## Ch01: Getting Started with Scratch
+
+### 01-01 Access Scratch on Your Computer
+
+#### 01-01-01 Create Online Account
+
+#### 01-01-02 Use Scratch Offline
+
+### 01-02 Create a New Project
+
+#### 01-02-01 Create Online Project
+
+#### 01-02-02 Create Offline Project
+
+#### 01-02-03 Delete the Cat
+
+#### 01-02-04 Choose Player Sprite
+
+#### 01-02-05 Paint Ground Sprite
+
+#### 01-02-06 Paint Pipe Sprite
+
+#### 01-02-07 Paint Sky Gradient on the Stage
+
+### 01-03 Bring Game to Life with Code
+
+#### 01-03-01 Add Flapping Wings Animation
+
+#### 01-03-02 Adjust Flapping Speed
+
+#### 01-03-03 Add Keyboard Controls
+
+#### 01-03-04 Add Gravity to Game
+
+#### 01-03-05 Make Pipe Move
+
+### 01-04 Add Collision to Your Game
+
+#### 01-04-01 Detect Collision with the Ground Sprite
+
+#### 01-04-02 Copy Blocks from Ground to Pipe
+
+### 01-05 Adjust Pipe Size and Location
+
+#### 01-05-01 Randomize Vertical Position
+
+#### 01-05-02 Increase Sprite Size on the Stage
+
+#### 01-05-03 Decrease Sprite Size on the Stage
+
+#### 01-05-04 Include Additional Pipes
+
+### 01-06 Wrapping Up the Flapping
+
+## Ch02: Create Your Own Comics
+
+## Ch03: Design Scratch Animals
+
+## Ch04: Build Vector Robots
+
+## Ch05: Digital Collages
+
+# Part 2: Become a Scratch Animator
+
+## Ch06: Animation Essentials
+
+## Ch07: Animate Great Characters
+
+## Ch08: Location, Location, Location
+
+## Ch09: Sounds Good t Me
+
+## Ch10: Lights, Camera, ACTION!
+
+# Part 3: Become a Scratch Game Developer
+
+## Ch11: Design a Classic Videogame
+
+## Ch12: Super Snake
+
+## Ch13: A-Maze-ing Game
+
+## Ch14: Attacking the Clones
+
+## Ch15: Game Not Over!
--- scratch4kids/Scratch4Kids.mm
+++ scratch4kids/Scratch4Kids.mm
@@ -1,6 +1,6 @@
 <map version="freeplane 1.11.5">
 <!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
-<node TEXT="Scratch for Kids" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1711296495428">
+<node TEXT="Scratch for Kids" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1711299172838">
 <hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
 <font BOLD="true"/>
 <hook NAME="MapStyle" background="#f9f9f8">
@@ -79,5 +79,88 @@
 </stylenode>
 </map_styles>
 </hook>
+<richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Author: Derek Breen (2nd Degree Black Belt in Scratch Ninjitsu)
+    </p>
+    <p>
+      Amazon.ca: https://www.amazon.ca/Scratch-Kids-Dummies-Derek-Breen/dp/1119014875/
+    </p>
+    <p>
+      Indigo.ca: https://www.indigo.ca/en-ca/scratch-for-kids-for-dummies/9781119014874.html
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Part 0: Introduction" POSITION="bottom_or_right" ID="ID_1041190190" CREATED="1711296950818" MODIFIED="1711296960185">
+<node TEXT="Get FreePlane" ID="ID_1772939036" CREATED="1711302707136" MODIFIED="1711302740542" LINK="https://sourceforge.net/projects/freeplane/"/>
+<node TEXT="About Scratch" FOLDED="true" ID="ID_385307101" CREATED="1711299989277" MODIFIED="1711300088524" LINK="https://scratch.mit.edu/">
+<node TEXT="1. Give you powerful software for free" ID="ID_649462176" CREATED="1711300007231" MODIFIED="1711300015392"/>
+<node TEXT="2. Make it easy for you to learn" ID="ID_959330937" CREATED="1711300015806" MODIFIED="1711300021846"/>
+<node TEXT="3. Allow many different ways for you to use the software" ID="ID_1490456903" CREATED="1711300022038" MODIFIED="1711300033263"/>
+<node TEXT="4. Enable you to browse/play/remix other projects" ID="ID_227020991" CREATED="1711300033423" MODIFIED="1711300045865"/>
+<node TEXT="5. Enable you to share your projects" ID="ID_891694779" CREATED="1711300046200" MODIFIED="1711300052080"/>
+<node TEXT="6. Create an online community where you can learn from one another" ID="ID_538706875" CREATED="1711300052224" MODIFIED="1711300067017"/>
+</node>
+<node TEXT="View the Book" ID="ID_1236838688" CREATED="1711300639373" MODIFIED="1711300646417" LINK="https://archive.org/details/scratchforkidsfo0000bree"/>
+</node>
+<node TEXT="Part 1: Become a Scratch Designer" FOLDED="true" POSITION="bottom_or_right" ID="ID_1616495337" CREATED="1711296960575" MODIFIED="1711296968417">
+<node TEXT="Ch01: Getting Started with Scratch" ID="ID_958938025" CREATED="1711297403336" MODIFIED="1711297420835">
+<node TEXT="01-01 Access Scratch on Your Computer" FOLDED="true" ID="ID_1058788285" CREATED="1711301610597" MODIFIED="1711301633357" LINK="https://scratch.mit.edu/">
+<node TEXT="01-01-01 Create Online Account" ID="ID_340025615" CREATED="1711301643526" MODIFIED="1711301651583"/>
+<node TEXT="01-01-02 Use Scratch Offline" ID="ID_405126313" CREATED="1711301757966" MODIFIED="1711301783698" LINK="https://scratch.mit.edu/download"/>
+</node>
+<node TEXT="01-02 Create a New Project" ID="ID_850116619" CREATED="1711301710389" MODIFIED="1711301718629">
+<node TEXT="01-02-01 Create Online Project" ID="ID_1479892256" CREATED="1711301798287" MODIFIED="1711301805800"/>
+<node TEXT="01-02-02 Create Offline Project" ID="ID_1742418307" CREATED="1711301811063" MODIFIED="1711301816952"/>
+<node TEXT="01-02-03 Delete the Cat" ID="ID_644331352" CREATED="1711301819766" MODIFIED="1711301826103"/>
+<node TEXT="01-02-04 Choose Player Sprite" ID="ID_253184777" CREATED="1711301833047" MODIFIED="1711301840416"/>
+<node TEXT="01-02-05 Paint Ground Sprite" ID="ID_1859354022" CREATED="1711301853160" MODIFIED="1711301863241"/>
+<node TEXT="01-02-06 Paint Pipe Sprite" ID="ID_1591134462" CREATED="1711301902002" MODIFIED="1711301908440"/>
+<node TEXT="01-02-07 Paint Sky Gradient on the Stage" ID="ID_1714281041" CREATED="1711301931112" MODIFIED="1711301940410"/>
+</node>
+<node TEXT="01-03 Bring Game to Life with Code" FOLDED="true" ID="ID_1949721986" CREATED="1711301718835" MODIFIED="1711301728864">
+<node TEXT="01-03-01 Add Flapping Wings Animation" ID="ID_381572951" CREATED="1711301951719" MODIFIED="1711301963497"/>
+<node TEXT="01-03-02 Adjust Flapping Speed" ID="ID_453338398" CREATED="1711301977023" MODIFIED="1711301983394"/>
+<node TEXT="01-03-03 Add Keyboard Controls" ID="ID_792212715" CREATED="1711301988351" MODIFIED="1711301995099"/>
+<node TEXT="01-03-04 Add Gravity to Game" ID="ID_291919104" CREATED="1711302000850" MODIFIED="1711302006891"/>
+<node TEXT="01-03-05 Make Pipe Move" ID="ID_1521376484" CREATED="1711302017847" MODIFIED="1711302024299"/>
+</node>
+<node TEXT="01-04 Add Collision to Your Game" FOLDED="true" ID="ID_1005247597" CREATED="1711301729054" MODIFIED="1711301736288">
+<node TEXT="01-04-01 Detect Collision with the Ground Sprite" ID="ID_640786455" CREATED="1711302030751" MODIFIED="1711302042536"/>
+<node TEXT="01-04-02 Copy Blocks from Ground to Pipe" ID="ID_1291457841" CREATED="1711302047632" MODIFIED="1711302059154"/>
+</node>
+<node TEXT="01-05 Adjust Pipe Size and Location" FOLDED="true" ID="ID_536694801" CREATED="1711301736583" MODIFIED="1711301745480">
+<node TEXT="01-05-01 Randomize Vertical Position" ID="ID_567748952" CREATED="1711302071416" MODIFIED="1711302082660"/>
+<node TEXT="01-05-02 Increase Sprite Size on the Stage" ID="ID_1687924912" CREATED="1711302088526" MODIFIED="1711302104411"/>
+<node TEXT="01-05-03 Decrease Sprite Size on the Stage" ID="ID_845749591" CREATED="1711302108769" MODIFIED="1711302118370"/>
+<node TEXT="01-05-04 Include Additional Pipes" ID="ID_444922754" CREATED="1711302121658" MODIFIED="1711302131249"/>
+</node>
+<node TEXT="01-06 Wrapping Up the Flapping" ID="ID_1574228199" CREATED="1711301745644" MODIFIED="1711301753016"/>
+</node>
+<node TEXT="Ch02: Create Your Own Comics" ID="ID_1074144711" CREATED="1711297421153" MODIFIED="1711297428480"/>
+<node TEXT="Ch03: Design Scratch Animals" ID="ID_1851907459" CREATED="1711297429233" MODIFIED="1711297436827"/>
+<node TEXT="Ch04: Build Vector Robots" ID="ID_452942753" CREATED="1711298913524" MODIFIED="1711298921917"/>
+<node TEXT="Ch05: Digital Collages" ID="ID_175698884" CREATED="1711298922107" MODIFIED="1711298930151"/>
+</node>
+<node TEXT="Part 2: Become a Scratch Animator" FOLDED="true" POSITION="bottom_or_right" ID="ID_1689479168" CREATED="1711296973070" MODIFIED="1711296981220">
+<node TEXT="Ch06: Animation Essentials" ID="ID_74823263" CREATED="1711298938836" MODIFIED="1711298945797"/>
+<node TEXT="Ch07: Animate Great Characters" ID="ID_58658302" CREATED="1711298946003" MODIFIED="1711298953902"/>
+<node TEXT="Ch08: Location, Location, Location" ID="ID_1296188955" CREATED="1711298954091" MODIFIED="1711298965006"/>
+<node TEXT="Ch09: Sounds Good t Me" ID="ID_1717156766" CREATED="1711298984356" MODIFIED="1711299077520"/>
+<node TEXT="Ch10: Lights, Camera, ACTION!" ID="ID_792208315" CREATED="1711298986965" MODIFIED="1711298995885"/>
+</node>
+<node TEXT="Part 3: Become a Scratch Game Developer" FOLDED="true" POSITION="bottom_or_right" ID="ID_1970165623" CREATED="1711296984433" MODIFIED="1711296992923">
+<node TEXT="Ch11: Design a Classic Videogame" ID="ID_1232977729" CREATED="1711298998304" MODIFIED="1711299007549"/>
+<node TEXT="Ch12: Super Snake" ID="ID_1649275452" CREATED="1711299007693" MODIFIED="1711299011637"/>
+<node TEXT="Ch13: A-Maze-ing Game" ID="ID_719752155" CREATED="1711299011812" MODIFIED="1711299024893"/>
+<node TEXT="Ch14: Attacking the Clones" ID="ID_1682343688" CREATED="1711299025197" MODIFIED="1711299030726"/>
+<node TEXT="Ch15: Game Not Over!" ID="ID_458884026" CREATED="1711299033165" MODIFIED="1711299038439"/>
+</node>
 </node>
 </map>
